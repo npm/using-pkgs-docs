1c3a88a8fb4cb35540920457d4eb0b0ad6840905
darcyclarke <darcy@darcyclarke.me>
2020-04-07 12:05:10 -0400

    fix: remove personal credentials

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ rev=$(git rev-parse --short HEAD)
 cd _book
 
 git init
-git config user.name "Ashley Williams"
-git config user.email "ashley666ashley@gmail.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/npm/using-pkgs-docs.git"
 git fetch upstream
